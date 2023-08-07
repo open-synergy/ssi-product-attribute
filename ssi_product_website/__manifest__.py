f790a59cbb4052260d8320bfcacf702c1750d08b
Miftahussalam <miftah_salam@ymail.com>
2023-08-07 23:37:41 +0700

    [ssi_product_website] 14.0.1.0.0

---
+++ ssi_product_website/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Product App - website Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "auto_install": True,
+    "depends": [
+        "ssi_product",
+        "website",
+        "website_sale",
+    ],
+    "data": [
+        # "views/templates.xml",
+        "data/data.xml",
+    ],
+    "demo": [],
+}
