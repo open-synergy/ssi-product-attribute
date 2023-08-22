70861f1f66610b6132085201704c5a60eae1ed65
Miftahussalam <miftah_salam@ymail.com>
2023-08-23 00:31:54 +0700

    [ssi_product_website] 14.0.1.0.0

--- ssi_product_website/static/src/js/tours/website_sale_shop_backend.js
+++
@@ -1,8 +0,0 @@
-odoo.define("ssi_product_website.tour_shop_backend", function (require) {
-"use strict";
-
-var tour = require("web_tour.tour");
-var steps = require("ssi_product_website.tour_shop");
-tour.register("product_catalog", {url: "/product_catalog"}, steps);
-
-});
